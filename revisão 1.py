44dca87f0f108b47c64c6b54a96875051d0019d4
VITOR HUGO AMARO ARISTIDES <vitor.haa@puccampinas.edu.br>
2025-02-25 09:40:23 -0300

    python revisao 1 tipos primitivos, operacoes e declaracao de variaveis

---
+++ revisão 1.py
@@ -0,0 +1,91 @@
+
+"""
+
+ -> Tipos de Dados
+
+1 - numericos
+reais
+inteiros
+
+2 - Logicos
+true
+false
+
+3 - nao numericos
+textos(cadeia de caracteres)
+
+
+ -> Operadores aritimeticos
+
++  soma
+-  subtracao
+*  multiplicacao
+/  divisao
+// quocientge
+%  resto
+** expoenciacao
+** radiacao  na versao 2.7 tem que declar o numerador como float
+
+
+ -> Procedencia de Operadores
+
+1 parenteses
+2 potenciacao
+3 multiplicacao
+4 soma e subtracao
+
+"""
+#adicao
+print(3 + 2)
+
+#subtracao
+print(7-5)
+
+#multiplicacao
+print(2 * 3)
+print(3 * 3.5)
+print(4 * 5.0)
+
+#divisao
+print(10/3)
+print(9/3.0)
+
+
+#divisao calculando o coeficiente
+print(5//2)
+print(5.0//2)
+print(4//1.2)
+
+#divisao calculando o resto
+print(9% 2.5)
+print(9%2.5)
+
+#potenciacao: exponenciacao
+print(3**2)
+print(2 ** 10)
+
+#potenciacao radiciacao
+print(9 ** (float(1)/2))
+#elevando em decimal ao inves de fracao
+print(16 ** 0.5)
+
+#raiz cubica
+print(27 ** (float(1)/3))
+
+
+# excercicios
+# 2+3*4/5-6
+print(2 + 3 * 4/5 - 6)
+print((2+3)*4/(5-6))
+
+#raiz quadrada de 2 - 3 elevado 2, dividido por 4
+print(((2-3)**float(2)/4)**0.5)
+
+"""
+Variaveis e comandos de atribuicao 
+
+identificadores devem comecar ou com letras ou underscore, sem espacos caracteres especiais.
+
+"""
+
+
