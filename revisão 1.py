edf845745502d7c4535e322e5a52fb4322648f63
VITOR HUGO AMARO ARISTIDES <vitor.haa@puccampinas.edu.br>
2025-02-26 12:47:15 -0300

    rervisao 01

--- revisão 1.py
+++ revisão 1.py
@@ -1,91 +1,17 @@
+#Cálculo da área e comprimento da circunfêrencia 
+#comprimento = 2 x pi*raio
 
-"""
-
- -> Tipos de Dados
-
-1 - numericos
-reais
-inteiros
-
-2 - Logicos
-true
-false
-
-3 - nao numericos
-textos(cadeia de caracteres)
+raio = float(input("digite o valor do raio"))
+area = 3.14159 * raio ** 2
+comprimento = 2 * 3.14159 * raio
+print('Para o raio {},  área = {:.3f} e o comprimento = {}'.format(raio, area, comprimento))
 
 
- -> Operadores aritimeticos
 
-+  soma
--  subtracao
-*  multiplicacao
-/  divisao
-// quocientge
-%  resto
-** expoenciacao
-** radiacao  na versao 2.7 tem que declar o numerador como float
-
-
- -> Procedencia de Operadores
-
-1 parenteses
-2 potenciacao
-3 multiplicacao
-4 soma e subtracao
-
-"""
-#adicao
-print(3 + 2)
-
-#subtracao
-print(7-5)
-
-#multiplicacao
-print(2 * 3)
-print(3 * 3.5)
-print(4 * 5.0)
-
-#divisao
-print(10/3)
-print(9/3.0)
-
-
-#divisao calculando o coeficiente
-print(5//2)
-print(5.0//2)
-print(4//1.2)
-
-#divisao calculando o resto
-print(9% 2.5)
-print(9%2.5)
-
-#potenciacao: exponenciacao
-print(3**2)
-print(2 ** 10)
-
-#potenciacao radiciacao
-print(9 ** (float(1)/2))
-#elevando em decimal ao inves de fracao
-print(16 ** 0.5)
-
-#raiz cubica
-print(27 ** (float(1)/3))
-
-
-# excercicios
-# 2+3*4/5-6
-print(2 + 3 * 4/5 - 6)
-print((2+3)*4/(5-6))
-
-#raiz quadrada de 2 - 3 elevado 2, dividido por 4
-print(((2-3)**float(2)/4)**0.5)
-
-"""
-Variaveis e comandos de atribuicao 
-
-identificadores devem comecar ou com letras ou underscore, sem espacos caracteres especiais.
-
-"""
-
-
+# Calculando a distância entre os pontos (3,4) e (5,6)
+x1 = 3
+y1 = 4
+x2 = 5
+y2 = 6
+dist = ((x1-x2)**2 + (y1-y2)**2)**(1/2)
+print('A distância entre os pontos({},{}) e ({},{}) = {}.'.format(x1, y1, x2, y2, dist))
